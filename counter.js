6de206cd3faa009b7b13020b9bf3c6489e013557
jeremia-rantsho <rantshodiketso17@gmail.com>
2024-07-12 10:29:40 +0200

    added range for count variable

--- counter.js
+++ counter.js
@@ -3,11 +3,23 @@ function Counter() {
     return {
         count: 0,
 
+
         increment() {
-            this.count++;
+            if (this.count >= 5) {
+                this.count = 5;
+            } else {
+                this.count++;
+            }
+
+
         },
         decrement() {
-            this.count--;
+            if (this.count <= 0) {
+                this.count = 0;
+            } else {
+                this.count--;
+            }
+
         },
         reset() {
             this.count = 0;
